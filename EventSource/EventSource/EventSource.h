5fefd8ea45b11e46079e9474b7b71616a8b2edfa
Achim Breidenbach <achim@boinx.com>
2018-05-09 13:04:49 +0200

    made project build for macOS

--- EventSource/EventSource/EventSource.h
+++ EventSource/EventSource/EventSource.h
@@ -6,13 +6,13 @@
 //  Copyright © 2018 inaka. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+// #import <UIKit/UIKit.h>
 
 //! Project version number for EventSource.
-FOUNDATION_EXPORT double EventSourceVersionNumber;
+// FOUNDATION_EXPORT double EventSourceVersionNumber;
 
 //! Project version string for EventSource.
-FOUNDATION_EXPORT const unsigned char EventSourceVersionString[];
+// FOUNDATION_EXPORT const unsigned char EventSourceVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <EventSource/PublicHeader.h>
 
